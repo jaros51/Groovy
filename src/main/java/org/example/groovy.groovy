0992871e703e63d3517b6b1de586fc8f2223f783
jaros51 <jaros51@hotmail.com>
2023-07-02 03:31:14 +0200

    2

--- src/main/java/org/example/groovy.groovy
+++ src/main/java/org/example/groovy.groovy
@@ -44,11 +44,93 @@ println m.Google
 println m["Amazon"]
 println m["Google"]
 
+//////////////////////////////
 
-
-
+def str = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor."
+def result1 = (str =~ /\b[A-Za-z]/)
+    .replaceAll{match ->
+      "[${match.group()}]".toString()
+}
+println(result1)
 ///////////////////////////////
 
+
+abstract class Something {
+    abstract void foo()
+    abstract void bar()
+}
+
+Something something = [
+        foo: { println "Ffoo" },
+        bar: { println "Bbar" }
+] as Something
+
+something.foo()
+something.bar()
+
+////////////////////////////////
+
+Runnable runnable = {
+    println "Im koko"
+}
+
+runnable.run()
+
+
+
+////////////////////////////////
+
+def sum = (0..20).inject(0) { acc, n-> acc + n }
+
+println sum
+
+def product = (1..10).inject(1) { acc, n-> acc * n }
+
+println product
+
+////////////////////////////////
+
+
+def closure = {
+    add(1)
+    add(2)
+}
+
+def list =  []
+
+closure.delegate = list
+closure.resolveStrategy = Closure.DELEGATE_ONLY
+closure()
+
+println list
+
+
+
+////////////////////////////////
+
+def filename =  "c:/temp/${UUID.randomUUID().toString()}" /// TODO !!! WIN / MAC
+
+new File(filename) << """
+${UUID.randomUUID().toString()}
+${UUID.randomUUID().toString()}
+${UUID.randomUUID().toString()}
+""".trim()
+
+new File(filename).eachLine { line ->
+    println line
+}
+////////////////////////////////
+
+
+//System.in.eachLine { line ->
+//    println line.toUpperCase()
+//
+//}
+System.in.eachByte { byte b->
+    println b
+}
+
+////////////////////////////////
 
 
 class Example {
